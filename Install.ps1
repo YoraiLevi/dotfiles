5d17cf33f7e2b7ab4d4ecb92fde50b86c5dba1e1
Yorai <yorai.email@gmail.com>
2024-06-14 21:08:31 +0300

    sure?

--- Install.ps1
+++ Install.ps1
@@ -1,3 +1,6 @@
+if ($GITHUB_USERNAME -eq $null){
+    throw "Github username variable isn't set!"
+}
 function Test-Administrator {  
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
@@ -10,4 +13,4 @@ function Throw-NotAdministrator {
 Throw-NotAdministrator -ErrorAction Stop
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install pwsh -y # this must be bootstrapped outside of chezmoi for some reason!
-powershell.exe -NoProfile -Command {$GITHUB_USERNAME = "YoraiLevi"; cd $HOME; iex "&{$(irm 'https://get.chezmoi.io/ps1')} init --apply 'YoraiLevi'"}
+iex "&{$(irm 'https://get.chezmoi.io/ps1')} -BinDir '$HOME' init --apply '$GITHUB_USERNAME'"
