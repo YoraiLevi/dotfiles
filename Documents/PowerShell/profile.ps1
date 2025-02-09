719ce33a8db432544ca5c4a40ab0147722ce9db8
Yorai <yorai.email@gmail.com>
2025-02-09 23:23:27 +0200

    Update Documents/PowerShell/profile.ps1

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -189,13 +189,12 @@ function Invoke-Profile {
     ) | ForEach-Object {
         if (Test-Path $_) {
             Write-Verbose "Running $_"
-            cat $_ | iex
+            . $_ 
         }
     }
 }
 Set-Alias -Name 'Reload-Profile' -Value Invoke-Profile
 # Quick Access to Editing the Profile
-
 
 # function Edit-Profile([switch]$Reload = $True, [switch]$EditChezmoi = $True, [string]$PowerShellProfile = $Profile.CurrentUserAllHosts) {
 #     # todo add check one of available profiles
@@ -515,3 +514,6 @@ function Invoke-Conda {
     conda @args
 }
 Set-Alias -Name conda -Value Invoke-Conda -Scope Global
+
+Set-Alias -Name sudo -Value gsudo
+$A = 1
