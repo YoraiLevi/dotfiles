6081c8c8b30b75dd633054e08edd2211b84faf63
Yorai <yorai.email@gmail.com>
2025-09-26 21:16:11 +0300

    Update .PowerShell/profile.ps1

--- dot_PowerShell/profile.ps1
+++ dot_PowerShell/profile.ps1
@@ -133,7 +133,7 @@ function Invoke-YesNoPrompt {
     }
 }
 # Update local changes to chezmoi repo
-&$ENV:_EDITOR --list-extensions > $ENV:USERPROFILE\.vscode\$ENV:_EDITOR-extensions.txt
+# &$ENV:_EDITOR --list-extensions > $ENV:USERPROFILE\.vscode\$ENV:_EDITOR-extensions.txt
 $null = (chezmoi re-add)
 # weekly update check
 if ($(try { Get-Date -Date (Get-Content "$PSScriptRoot/date.tmp" -ErrorAction SilentlyContinue) }catch {}) -lt $(Get-Date)) {
