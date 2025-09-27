750deccf71be950698039bdd2de9e7ed39e587f5
Yorai <yorai.email@gmail.com>
2025-09-27 07:30:51 +0300

    Update .PowerShell/profile.ps1

--- dot_PowerShell/profile.ps1
+++ dot_PowerShell/profile.ps1
@@ -869,7 +869,8 @@ function Invoke-Chezmoi {
 Set-Alias -Name chezmoi -Value Invoke-Chezmoi -Scope Global
 
 Write-Host $chezmoi_process.ExitCode
-# $c = $chezmoi_process.StandardOutput.Read()
+$c = $chezmoi_process.StandardOutput.Read()
+Write-Host $c
 # if ($null -ne $c -and $c -ne -1 ) {
 #     do {
 #         write-host "$([char]$c)" -NoNewline
