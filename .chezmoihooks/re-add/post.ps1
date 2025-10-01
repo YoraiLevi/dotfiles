8721f4fbf0637f741577e1d7314e05f1174ed740
Yorai <yorai.email@gmail.com>
2025-10-01 15:08:24 +0300

    Update .chezmoihooks/re-add/post.ps1

--- .chezmoihooks/re-add/post.ps1
+++ .chezmoihooks/re-add/post.ps1
@@ -9,8 +9,11 @@ if ("--debug" -in $ENV:CHEZMOI_ARGS) {
 if ("--verbose" -in $ENV:CHEZMOI_ARGS) {
     $params += "--verbose"
 }
+if (("--dry-run" -in $ENV:CHEZMOI_ARGS) -or ("-n" -in $ENV:CHEZMOI_ARGS)) {
+    $params += "--dry-run"
+}
 try {
-    # & $ENV:CHEZMOI_EXECUTABLE init @params
+    & $ENV:CHEZMOI_EXECUTABLE init @params
 }
 catch {
     Write-Error "Failed to invoke chezmoi.exe for $dirPath. Error: $_"
