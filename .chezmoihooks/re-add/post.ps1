60abf5090c340318e51072e59b021930d30ce0fd
Yorai <yorai.email@gmail.com>
2025-10-02 21:00:56 +0300

    Update .chezmoihooks/re-add/post.ps1

--- .chezmoihooks/re-add/post.ps1
+++ .chezmoihooks/re-add/post.ps1
@@ -1,4 +1,4 @@
 param()
 Write-Host $PSCommandPath -ForegroundColor Green
 $ErrorActionPreference = 'Stop'
-Get-ChildItem Env: | Where-Object { $_.Name -like 'CHEZMOI*' } | ForEach-Object { Write-Debug $_.Name, $_.Value -ForegroundColor Yellow }
+Get-ChildItem Env: | Where-Object { $_.Name -like 'CHEZMOI*' } | ForEach-Object { Write-Host $_.Name, $_.Value -ForegroundColor Yellow }
