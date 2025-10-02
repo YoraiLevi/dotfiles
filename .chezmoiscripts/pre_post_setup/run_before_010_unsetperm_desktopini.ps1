0b910b1da414f8cfdfffcd749a0601e07f8f706c
Yorai <yorai.email@gmail.com>
2025-10-02 20:36:19 +0300

    Update .chezmoiscripts/pre_post_setup/010_setperm_desktopini.ps1 Update .chezmoiscripts/pre_post_setup/010_unsetperm_desktopini.ps1

--- .chezmoiscripts/pre_post_setup/run_before_010_unsetperm_desktopini.ps1
+++ .chezmoiscripts/pre_post_setup/run_before_010_unsetperm_desktopini.ps1
@@ -1,3 +1,5 @@
+echo "== {{ .chezmoi.sourceFile | trim }} =="
+
 Import-Module (Join-Path $ENV:CHEZMOI_WORKING_TREE .chezmoilib\DesktopIniAttributes.psm1)
 
 Get-ChildItem -Path $ENV:CHEZMOI_DEST_DIR -Filter desktop.ini -Recurse | ForEach-Object {
