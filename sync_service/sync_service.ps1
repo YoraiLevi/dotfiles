88325da05be1316d55105cb42bc7cd75573624e4
Yorai <yorai.email@gmail.com>
2026-02-01 09:14:16 +0200

    Update .gitignore Update sync_service/sync_service.ps1

--- sync_service/sync_service.ps1
+++ sync_service/sync_service.ps1
@@ -427,7 +427,9 @@ Chezmoi path: $ChezmoiPath
         }
         $Chezmoi_diff = $(& $ChezmoiPath git pull -- --autostash --rebase) | Out-String
         $NoChanges = 'Current branch master is up to date.', 'Already up to date.'
-        if (-not (([string]$Chezmoi_diff).trim() -in $NoChanges)) {
+    
+        if ((-not (([string]$Chezmoi_diff).trim() -in $NoChanges)) -or ($(try { Get-Date -Date (Get-Content "$PSScriptRoot/date.tmp" -ErrorAction SilentlyContinue) }catch {}) -lt $(Get-Date))) {
+            (Get-Date).Date.AddHours(6).DateTime > "$PSScriptRoot/date.tmp"
             # Execute chezmoi update --init --apply --force
             & $ChezmoiPath update --init --apply --force 2>&1
             $exitCode = $LASTEXITCODE
