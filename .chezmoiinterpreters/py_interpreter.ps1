64ee26175a1814439e34735386373af6887f72bf
Yorai <yorai.email@gmail.com>
2026-02-07 05:31:39 +0200

    Update .chezmoi.yaml Update .chezmoiinterpreters/py_interpreter.ps1

--- .chezmoiinterpreters/py_interpreter.ps1
+++ .chezmoiinterpreters/py_interpreter.ps1
@@ -2,6 +2,6 @@ param()
 if ($null -ne $ENV:CHEZMOI_SOURCE_FILE) {
     Write-Host "== $ENV:CHEZMOI_SOURCE_FILE ==" -ForegroundColor Green
 }
-
+Write-Host "args: $args" -ForegroundColor Cyan
 $ENV:CHEZMOI_DATA = (chezmoi data --format json | Out-String | ConvertFrom-Json)
 & $args
