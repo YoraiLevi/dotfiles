e324a8f95e81fa1033903a6c292bf97d3e09ac66
Yorai <yorai.email@gmail.com>
2026-02-07 04:20:58 +0200

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,17 +1,17 @@
-if ($GITHUB_USERNAME -eq $null){
+if ($GITHUB_USERNAME -eq $null) {
     throw "Github username variable isn't set!"
 }
 function Test-Administrator {  
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
-function Throw-NotAdministrator {
+function Test-NotAdministrator {
     if ((Test-Administrator) -eq $false) {
         throw "Access denied. Please run as an administrator."
     }
 }
-Throw-NotAdministrator -ErrorAction Stop
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Test-NotAdministrator -ErrorAction Stop
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install pwsh -y # this must be bootstrapped outside of chezmoi for some reason!
-iex "&{$(irm 'https://get.chezmoi.io/ps1')} -BinDir '$HOME/.local/bin/' init --apply '$GITHUB_USERNAME'" | Tee-Object -FilePath $Home/CHEZMOI.log -Append
-cd (Join-Path $(chezmoi source-path) sync_service) && .\sync_service.ps1 -Install
+Invoke-Expression "&{$(Invoke-RestMethod 'https://get.chezmoi.io/ps1')} -BinDir '$HOME/.local/bin/' init --apply '$GITHUB_USERNAME'" | Tee-Object -FilePath $Home/CHEZMOI.log -Append
+Set-Location (Join-Path $(chezmoi source-path) sync_service) && .\sync_service.ps1 -Install
