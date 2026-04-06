b74bb5d900614cbdce81d8de2ac2b3fab0955d3e
Yorai <yorai.email@gmail.com>
2026-04-06 19:16:10 +0300

    Add .chezmoiscripts/pre_post_setup/001_wsl2_home_symlink.sh

---
+++ .chezmoiscripts/pre_post_setup/run_before_001_wsl2_home_symlink.sh
@@ -0,0 +1,15 @@
+# windows -> wsl2:
+WinHome="$(wslpath -u "$(cmd.exe /c "echo %USERPROFILE%" | tr -d '\r' | sed 's|\\|/|g')")/.wsl2/home/"
+
+if [ -d "$WinHome" ]; then
+  find "$WinHome" -type f | while read -r file; do
+    rel="${file#"$WinHome"}"
+    target="$HOME/$rel"
+    echo "$rel"
+    mkdir -p "$(dirname "$target")"
+    if [ ! -L "$target" ]; then
+      [ -e "$target" ] && mv -f "$target" "$target.bak"
+      ln -sf "$file" "$target"
+    fi
+  done
+fi
