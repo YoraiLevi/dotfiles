29d9ab1ec18fc34f237f5cbafa54952608210ce1
Yorai <yorai.email@gmail.com>
2026-05-21 01:38:00 +0300

    chore(dotfiles): mod .powershell/profile.ps1 at 2026-05-21T01:37:59.5640076+03:00
    Modified:
      .powershell/profile.ps1

--- .powershell/profile.ps1
+++ .powershell/profile.ps1
@@ -1143,7 +1143,7 @@ function Open-ObsidianVault {
         throw "No registered Obsidian vault found for: $($Vault ?? (Get-Location).Path)"
     }
 
-    Start-Process "obsidian://open?path=$([uri]::EscapeDataString($match.Path))" -Wait -NoNewWindow
+    Start-Process "obsidian://open?path=$([uri]::EscapeDataString($match.Path))"
     return
 }
 
