9f1c0aaad1e45bb34c4b9ccb9a7de0512545821e
Yorai <yorai.email@gmail.com>
2026-05-24 01:28:56 +0300

    chore(dotfiles): mod .powershell/profile.ps1 at 2026-05-24T01:28:55.5926989+03:00
    Modified:
      .powershell/profile.ps1

--- .powershell/profile.ps1
+++ .powershell/profile.ps1
@@ -1584,6 +1584,7 @@ function Invoke-Claude {
         Set-Location $tmpDir
     }
     $ENV:CLAUDE_CODE_BLOCKING_LIMIT_OVERRIDE = 10 * 1024 * 1024
+    [Console]::Write("`e[?1003l`e[?1002l`e[?1000l`e[?1006l")
     & claude.exe --enable-auto-mode --allow-dangerously-skip-permissions --strict-mcp-config @Args
     Set-Location $currentPath
 }
