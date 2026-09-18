fea487b35ae83649a031958b202455936b9ad0fe
Yorai <yorai.email@gmail.com>
2026-09-18 16:46:54 +0300

    Fix bootstrap script to copy shell configuration files only if they exist
    - Updated the bootstrap script to copy shell configuration files (.bashrc, .bash_profile, .inputrc, .profile, .zshrc, .zprofile, .zlogin, .zlogout) only if they are present in the current directory, ensuring that existing user configurations are not overwritten.

--- .local/bin/devcontainers/bootstrap-devcontainer.sh
+++ .local/bin/devcontainers/bootstrap-devcontainer.sh
@@ -6,27 +6,27 @@ git checkout --detach master
 echo "Bootstrapping devcontainer..."
 echo $(pwd)
 
-if [ ! -f ./.bashrc ]; then
+if [ -f ./.bashrc ]; then
   cp ./.bashrc ~/.bashrc
 fi
-if [ ! -f ./.bash_profile ]; then
+if [ -f ./.bash_profile ]; then
   cp ./.bash_profile ~/.bash_profile
 fi
-if [ ! -f ./.inputrc ]; then
+if [ -f ./.inputrc ]; then
   cp ./.inputrc ~/.inputrc
 fi
-if [ ! -f ./.profile ]; then
+if [ -f ./.profile ]; then
   cp ./.profile ~/.profile
 fi
-if [ ! -f ./.zshrc ]; then
+if [ -f ./.zshrc ]; then
   cp ./.zshrc ~/.zshrc
 fi
-if [ ! -f ./.zprofile ]; then
+if [ -f ./.zprofile ]; then
   cp ./.zprofile ~/.zprofile
 fi
-if [ ! -f ./.zlogin ]; then
+if [ -f ./.zlogin ]; then
   cp ./.zlogin ~/.zlogin
 fi
-if [ ! -f ./.zlogout ]; then
+if [ -f ./.zlogout ]; then
   cp ./.zlogout ~/.zlogout
 fi 
