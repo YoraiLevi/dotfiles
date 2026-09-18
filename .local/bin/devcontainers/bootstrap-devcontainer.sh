abfd74b523129df7c3414dcdee38efa6eb0e6934
Yorai <yorai.email@gmail.com>
2026-09-18 10:56:16 +0300

    Enhance bootstrap script for devcontainer setup
    - Added functionality to copy default shell configuration files (.bashrc, .bash_profile, .inputrc, .profile, .zshrc, .zprofile, .zlogin, .zlogout) if they do not already exist in the user's home directory.
    - Included an echo statement to indicate the start of the bootstrapping process.

--- .local/bin/devcontainers/bootstrap-devcontainer.sh
+++ .local/bin/devcontainers/bootstrap-devcontainer.sh
@@ -4,4 +4,29 @@ set -euo pipefail
 git fetch --depth 1 origin devcontainer-linux
 git checkout --detach FETCH_HEAD
 
-exec ./bootstrap-devcontainer.sh
+echo "Bootstrapping devcontainer..."
+
+if [ ! -f ~/.bashrc ]; then
+  cp ./bashrc ~/.bashrc
+fi
+if [ ! -f ~/.bash_profile ]; then
+  cp ./bash_profile ~/.bash_profile
+fi
+if [ ! -f ~/.inputrc ]; then
+  cp ./inputrc ~/.inputrc
+fi
+if [ ! -f ~/.profile ]; then
+  cp ./profile ~/.profile
+fi
+if [ ! -f ~/.zshrc ]; then
+  cp ./zshrc ~/.zshrc
+fi
+if [ ! -f ~/.zprofile ]; then
+  cp ./zprofile ~/.zprofile
+fi
+if [ ! -f ~/.zlogin ]; then
+  cp ./zlogin ~/.zlogin
+fi
+if [ ! -f ~/.zlogout ]; then
+  cp ./zlogout ~/.zlogout
+fi 
