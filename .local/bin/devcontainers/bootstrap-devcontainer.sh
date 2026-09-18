15cc03ab3526035db5f46379342471939fa4b573
Yorai <yorai.email@gmail.com>
2026-09-18 13:50:57 +0300

    Refactor bootstrap script to ensure correct file paths for shell configuration
    - Updated the bootstrap script to use relative paths for copying shell configuration files (.bashrc, .bash_profile, .inputrc, .profile, .zshrc, .zprofile, .zlogin, .zlogout) to the user's home directory.
    - Added an echo statement to display the current working directory during the bootstrapping process.

--- .local/bin/devcontainers/bootstrap-devcontainer.sh
+++ .local/bin/devcontainers/bootstrap-devcontainer.sh
@@ -4,28 +4,29 @@ set -euo pipefail
 git checkout --detach master
 
 echo "Bootstrapping devcontainer..."
+echo $(pwd)
 
-if [ ! -f ~/.bashrc ]; then
-  cp ./bashrc ~/.bashrc
+if [ ! -f ./.bashrc ]; then
+  cp ./.bashrc ~/.bashrc
 fi
-if [ ! -f ~/.bash_profile ]; then
-  cp ./bash_profile ~/.bash_profile
+if [ ! -f ./.bash_profile ]; then
+  cp ./.bash_profile ~/.bash_profile
 fi
-if [ ! -f ~/.inputrc ]; then
-  cp ./inputrc ~/.inputrc
+if [ ! -f ./.inputrc ]; then
+  cp ./.inputrc ~/.inputrc
 fi
-if [ ! -f ~/.profile ]; then
-  cp ./profile ~/.profile
+if [ ! -f ./.profile ]; then
+  cp ./.profile ~/.profile
 fi
-if [ ! -f ~/.zshrc ]; then
-  cp ./zshrc ~/.zshrc
+if [ ! -f ./.zshrc ]; then
+  cp ./.zshrc ~/.zshrc
 fi
-if [ ! -f ~/.zprofile ]; then
-  cp ./zprofile ~/.zprofile
+if [ ! -f ./.zprofile ]; then
+  cp ./.zprofile ~/.zprofile
 fi
-if [ ! -f ~/.zlogin ]; then
-  cp ./zlogin ~/.zlogin
+if [ ! -f ./.zlogin ]; then
+  cp ./.zlogin ~/.zlogin
 fi
-if [ ! -f ~/.zlogout ]; then
-  cp ./zlogout ~/.zlogout
+if [ ! -f ./.zlogout ]; then
+  cp ./.zlogout ~/.zlogout
 fi 
